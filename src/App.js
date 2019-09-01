bf9ffccb572eaf590a40a4a9921608b7e802f30d
David <davidola360@gmail.com>
2019-09-01 01:18:19 +0100

    added form validation and made stories dynamic

--- src/App.js
+++ src/App.js
@@ -4,14 +4,159 @@ import {CSSTransition} from 'react-transition-group';
 
 class App extends Component {
   state = {
-    isModalOpen: true
+    isModalOpen: false,
+    modalType: '',
+    stories: [
+      {
+        title: 'Love at First Sight',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper luctus quam, quis tempus tortor bibendum in alterest',
+        name: 'David Adeoye',
+        username: 'davidola',
+        initials: 'D.A',
+        banner: "/img/inlove.jpg",
+        avatar: "/img/smile.png",
+        voteCount: 60,
+        id: 1
+      },
+      {
+        title: 'A day in with the stars',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper luctus quam, quis tempus tortor bibendum in alterest Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper luctus quam, quis tempus tortor bibendum in alterest',
+        name: 'Ayo Afolabi',
+        username: 'ayo@startrek.co',
+        initials: 'A.A',
+        banner: "/img/inlove.jpg",
+        avatar: "/img/smile.png",
+        voteCount: 76,
+        id: 2
+      },
+      {
+        title: 'Dinner is all that counts',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper luctus quam, quis tempus tortor bibendum in alterest Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper luctus quam, quis tempus tortor bibendum in alterest',
+        name: 'Tunde Bakare',
+        username: 'tundeforshow@almightydollar.com',
+        initials: 'T.B',
+        banner: "/img/inlove.jpg",
+        avatar: "/img/smile.png",
+        voteCount: 65,
+        id: 3
+      }
+    ],
+    selectedStory: {},
+    form: {
+      name: '',
+      username: '',
+      description: '',
+      title: ''
+    },
+    errors: {
+      name: '',
+      username: '',
+      description: '',
+      title: ''
+    },
+    submitValidity: false,
   };
 
-  toggleState = e => {
-    this.setState({
-      isModalOpen: !this.state.isModalOpen
-    });
+  toggleState = (e,i) => {
+    if(e == 'view') {
+      this.setState({
+        isModalOpen: !this.state.isModalOpen,
+        modalType: e,
+        selectedStory: this.state.stories[i]
+      });
+    }else {
+      this.setState({
+        isModalOpen: !this.state.isModalOpen,
+        modalType: e,
+      });
+    }
+    
   };
+
+  resetForm = () => {
+    let form = {
+      name: '',
+      username: '',
+      description: '',
+      title: ''
+    }
+    this.setState({form:form,errors:form})
+  }
+
+  closeModal = e => {
+    this.setState({
+      isModalOpen: false
+    });
+    this.resetForm();
+  };
+
+  changeHandler = (event, el) => {
+    const updateForm = {...this.state.form};
+    updateForm[el] = event.target.value
+    let errors = {...this.state.errors}
+    if(el == 'description') {
+      errors.description = event.target.value.split(' ').length < 25 ? 'Description must be at least 25 words' : '';
+    }else{
+      errors[el] = event.target.value.length < 5 ? el + ' must be at leat 5 characters' : '';
+    }
+    this.setState({form:updateForm,errors:errors})
+  }
+
+  validateForm = () => {
+    let valid = true
+    let form = {...this.state.form}
+    let errors = {...this.state.errors}
+    let formArray = Object.values(form)
+    let errorArray = Object.values(errors)
+
+    formArray.forEach((el) =>{
+      if(el == ''){
+        valid = false
+      }
+    })
+    errorArray.forEach((el) =>{
+      if(el != ''){
+        valid = false
+      }
+    })
+    this.setState({submitValidity: !valid})
+    return valid
+  }
+
+  incrementVote = (i) => {
+    let stories = [...this.state.stories]
+    stories.forEach((el,index)=>{
+      if(el.id == i){
+        el.voteCount++
+      }
+    })
+    this.setState({
+      stories:stories
+    })
+  }
+
+  addStory = () => {
+    let valid = this.validateForm();
+    if(valid){
+      // console.log("The form is valid")
+      let initials = ""
+      let firstForm = {...this.state.form}
+      firstForm.name.split(' ').forEach((el,i)=>{
+        if(i == 0){
+          initials +=  el[0].toUpperCase()
+        }else {
+          initials += ('.' + el[0].toUpperCase())
+
+        }
+      })
+      let form = {...this.state.form, banner: "/img/inlove.jpg", avatar: "/img/smile.png", initials: initials,voteCount: 0,id: 'new' + Math.random()}
+      let stories = [...this.state.stories]
+      stories.push(form)
+      this.setState({stories:stories})
+      this.closeModal();
+    }else {
+    }
+  }
 
   componentDidMount() {
     window.addEventListener('scroll', function(e) {
@@ -42,14 +187,22 @@ class App extends Component {
             classNames="alert"
             unmountOnExit>
             <Modal
-            closeModal={this.toggleState}
+            closeModal={this.closeModal}
+            modalType={this.state.modalType}
+            story={this.state.selectedStory}
+            changed={this.changeHandler}
+            form={this.state.form}
+            incrementVote={this.incrementVote}
+            addStory={this.addStory}
+            errors={this.state.errors}
+            submitError={this.state.submitValidity}
             >
             </Modal>
             </CSSTransition>
         <nav className="navigation" id="navigation">
           <div className="row navigation-content">
               <h3 className="logo">Val Memoirs</h3>
-              <button onClick={this.toggleState} className="button">Share Your Story</button>
+              <button onClick={()=>{this.toggleState('form')}} className="button">Share Your Story</button>
           </div>
         </nav>
         <div className="banner" id="banner"></div>
@@ -64,126 +217,36 @@ class App extends Component {
           </div>
 
           <section className="stories">
-            <div className="story">
-                <div className="story__img-cont">
-                  <img src="/img/inlove.jpg" className="story__img"/>
-                </div>
-                <div className="story__content">
-                  <h4 className="story__content--title">Love at First Sight</h4>
-                  <p className="story__content--description">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper luctus quam, quis tempus tortor bibendum in
-                  </p>
-                  <div className="story__avatar">
-                      <img src="/img/smile.png" className="story__avatar--img"/>
-                      <span className="story__avatar--initials" >D.A</span>
+            {
+              this.state.stories.map((item,i)=>{
+                return(
+                  <div className="story" key={i}>
+                      <div className="story__img-cont">
+                        <img src={item.banner} className="story__img" alt="story banner"/>
+                      </div>
+                      <div className="story__content">
+                        <h4 className="story__content--title">{item.title}</h4>
+                        <p className="story__content--description">
+                        {item.description.substring(0, 100)}
+                        {
+                          item.description.length > 100  ?
+                            <span className="story__content--read" onClick={()=>{this.toggleState('view',i)}}>...Read more</span> : null
+                        }
+                        </p>
+                        <div className="story__avatar">
+                            <img src={item.avatar} className="story__avatar--img"/>
+                            <span className="story__avatar--initials" >{item.initials}</span>
+                        </div>
+                        <p className="story__personName"> {item.username} </p>
+                        <div className="story__button--cont">
+                          <button className="story__button" onClick={()=>{this.incrementVote(item.id)}}>VOTE</button>
+                          <span className="story__button--votes" >{item.voteCount} votes</span>
+                        </div>
+                      </div>
                   </div>
-                  <p className="story__personName">David <span className="story__personName-sub"> Adeoye</span> </p>
-                  <div className="story__button--cont">
-                    <button className="story__button">VOTE</button>
-                    <span className="story__button--votes">0 votes</span>
-                  </div>
-                </div>
-            </div>
-            <div className="story">
-                <div className="story__img-cont">
-                  <img src="/img/inlove.jpg" className="story__img"/>
-                </div>
-                <div className="story__content">
-                  <h4 className="story__content--title">Love at First Sight</h4>
-                  <p className="story__content--description">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper luctus quam, quis tempus tortor bibendum in
-                  </p>
-                  <div className="story__avatar">
-                      <img src="/img/smile.png" className="story__avatar--img"/>
-                      <span className="story__avatar--initials" >D.A</span>
-                  </div>
-                  <p className="story__personName">David <span className="story__personName-sub"> Adeoye</span> </p>
-                  <div className="story__button--cont">
-                    <button className="story__button">VOTE</button>
-                    <span className="story__button--votes">0 votes</span>
-                  </div>
-                </div>
-            </div>
-            <div className="story">
-                <div className="story__img-cont">
-                  <img src="/img/inlove.jpg" className="story__img"/>
-                </div>
-                <div className="story__content">
-                  <h4 className="story__content--title">Love at First Sight</h4>
-                  <p className="story__content--description">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper luctus quam, quis tempus tortor bibendum in
-                  </p>
-                  <div className="story__avatar">
-                      <img src="/img/smile.png" className="story__avatar--img"/>
-                      <span className="story__avatar--initials" >D.A</span>
-                  </div>
-                  <p className="story__personName">David <span className="story__personName-sub"> Adeoye</span> </p>
-                  <div className="story__button--cont">
-                    <button className="story__button">VOTE</button>
-                    <span className="story__button--votes">0 votes</span>
-                  </div>
-                </div>
-            </div>
-            <div className="story">
-                <div className="story__img-cont">
-                  <img src="/img/inlove.jpg" className="story__img"/>
-                </div>
-                <div className="story__content">
-                  <h4 className="story__content--title">Love at First Sight</h4>
-                  <p className="story__content--description">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper luctus quam, quis tempus tortor bibendum in
-                  </p>
-                  <div className="story__avatar">
-                      <img src="/img/smile.png" className="story__avatar--img"/>
-                      <span className="story__avatar--initials" >D.A</span>
-                  </div>
-                  <p className="story__personName">David <span className="story__personName-sub"> Adeoye</span> </p>
-                  <div className="story__button--cont">
-                    <button className="story__button">VOTE</button>
-                    <span className="story__button--votes">0 votes</span>
-                  </div>
-                </div>
-            </div>
-            <div className="story">
-                <div className="story__img-cont">
-                  <img src="/img/inlove.jpg" className="story__img"/>
-                </div>
-                <div className="story__content">
-                  <h4 className="story__content--title">Love at First Sight</h4>
-                  <p className="story__content--description">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper luctus quam, quis tempus tortor bibendum in
-                  </p>
-                  <div className="story__avatar">
-                      <img src="/img/smile.png" className="story__avatar--img"/>
-                      <span className="story__avatar--initials" >D.A</span>
-                  </div>
-                  <p className="story__personName">David <span className="story__personName-sub"> Adeoye</span> </p>
-                  <div className="story__button--cont">
-                    <button className="story__button">VOTE</button>
-                    <span className="story__button--votes">0 votes</span>
-                  </div>
-                </div>
-            </div>
-            <div className="story">
-                <div className="story__img-cont">
-                  <img src="/img/inlove.jpg" className="story__img"/>
-                </div>
-                <div className="story__content">
-                  <h4 className="story__content--title">Love at First Sight</h4>
-                  <p className="story__content--description">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper luctus quam, quis tempus tortor bibendum in
-                  </p>
-                  <div className="story__avatar">
-                      <img src="/img/smile.png" className="story__avatar--img"/>
-                      <span className="story__avatar--initials" >D.A</span>
-                  </div>
-                  <p className="story__personName">David <span className="story__personName-sub"> Adeoye</span> </p>
-                  <div className="story__button--cont">
-                    <button className="story__button">VOTE</button>
-                    <span className="story__button--votes">0 votes</span>
-                  </div>
-                </div>
-            </div>
+                )
+              })
+            }
             
           </section>
         </main>
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,34 +1,66 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
-import {CSSTransition} from 'react-transition-group';
 const modalRoot = document.getElementById("modal-root");
 
 class Modal extends Component {
-
+    
 
     render() {
         return ReactDom.createPortal(
             <div className="Modal">
                 
                 <div className="Modal-content">
-                    <form className="form">
+                    { this.props.modalType == 'form' ? <form className="form" onSubmit={this.props.addStory}>
                         <div className="form__title"><h3><span></span>Share Your Story</h3></div>
                         <label className="form__label">Fullname<span className="required"> *</span></label>
-                        <input type="text" className="form__input" placeholder="Enter fullname here" />
-                        <label className="form__label">Email<span className="required"> *</span></label>
-                        <input type="text" className="form__input" placeholder="Enter email here" />
+                        <input type="text" className="form__input" placeholder="Enter fullname "  value={this.props.form.name} onChange={(event)=>{this.props.changed(event, 'name')}}/>
+                        {this.props.errors.name == '' ? null : <label className="form__error">{this.props.errors.name}</label> }
+
+                        <label className="form__label">Username<span className="required"> *</span></label>
+                        <input type="text" className="form__input" placeholder="Enter username " value={this.props.form.username}  onChange={(event)=>{this.props.changed(event, 'username')}} />
+                        {this.props.errors.username == '' ? null : <label className="form__error">{this.props.errors.username}</label> }
                         <label className="form__label">Title of Story<span className="required"> *</span></label>
-                        <input type="text" className="form__input" placeholder="Enter title here" />
+                        <input type="text" className="form__input" placeholder="Enter title " value={this.props.form.title}  onChange={(event)=>{this.props.changed(event, 'title')}}/>
+                        {this.props.errors.title == '' ? null : <label className="form__error">{this.props.errors.title}</label> }
                         <label className="form__label">Description of Story<span className="required"> *</span></label>
-                        <textarea className="form__textarea" placeholder="Enter story description here"/>
-                        <label className="form__label">Add Banner<span className="required"> *</span></label>
+                        <textarea className="form__textarea" placeholder="Enter story description " value={this.props.form.description}  onChange={(event)=>{this.props.changed(event, 'description')}}/>
+                        {this.props.errors.description == '' ? null : <label className="form__error">{this.props.errors.description}</label> }
+                        {/* <label className="form__label">Add Banner<span className="required"> *</span></label>
                         <input type="file" placeholder="Add Banner" className="form__upload"/>
                         <label className="form__label">Add Avatar</label>
-                        <input type="file" placeholder="Add Avatar" className="form__upload"/>
+                        <input type="file" placeholder="Add Avatar" className="form__upload"/> */}
 
-                        <input type="button" value="Submit" className="form__button"/>
 
-                    </form>
+                        {this.props.submitError ? <p className="form__button--error">Ensure all required * fields have been filled</p> : null }
+                        <input type="button" value="Submit" className="form__button" onClick={this.props.addStory}/>
+
+                    </form>: null}
+
+                    {
+                        this.props.modalType == "view" ?
+                            <div>
+                               <div className="story" id="modal-story">
+                                <div className="story__img-cont">
+                                <img src="/img/inlove.jpg" className="story__img"/>
+                                </div>
+                                <div className="story__content">
+                                <h4 className="story__content--title">{this.props.story.title}</h4>
+                                <p className="story__content--description">
+                                    {this.props.story.description}
+                                </p>
+                                <div className="story__avatar">
+                                    <img src="/img/smile.png" className="story__avatar--img"/>
+                                    <span className="story__avatar--initials" >{this.props.story.initials}</span>
+                                </div>
+                                <p className="story__personName">{this.props.story.username} </p>
+                                <div className="story__button--cont">
+                                    <button className="story__button" onClick={()=>{this.props.incrementVote(this.props.story.id)}}>VOTE</button>
+                                    <span className="story__button--votes">{this.props.story.voteCount} votes</span>
+                                </div>
+                                </div>
+                            </div>
+                        </div> :null
+                    }
                 </div>
                 <div className="Modal-backdrop" onMouseDown={this.props.closeModal}></div>
             </div>, modalRoot
